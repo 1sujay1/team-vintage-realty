d330a04fa663f4f3ed6f4cde8bc872f288f8c1eb
logesh-aimwindow <Logeshwaran.p@aimwindow.co.in>
2025-06-21 12:56:37 +0530

    feat: email integration

--- js/contact.js
+++ js/contact.js
@@ -19,104 +19,128 @@ $(function() {
    jQuery Validate - Reset Defaults
    ================================================ */
 
-    $.validator.setDefaults({
-        ignore: [],
-        highlight: function(element) {
-            $(element).closest('.form-group').addClass('has-error');
-        },
-        unhighlight: function(element) {
-            $(element).closest('.form-group').removeClass('has-error');
-        },
-        errorElement: 'small',
-        errorClass: 'help-block',
-        errorPlacement: function(error, element) {
-            if (element.parent('.input-group').length || element.parent('label').length) {
-                error.insertAfter(element.parent());
-            } else {
-                error.insertAfter(element);
-            }
-        }
-    });
+    // $.validator.setDefaults({
+    //     ignore: [],
+    //     highlight: function(element) {
+    //         $(element).closest('.form-group').addClass('has-error');
+    //     },
+    //     unhighlight: function(element) {
+    //         $(element).closest('.form-group').removeClass('has-error');
+    //     },
+    //     errorElement: 'small',
+    //     errorClass: 'help-block',
+    //     errorPlacement: function(error, element) {
+    //         if (element.parent('.input-group').length || element.parent('label').length) {
+    //             error.insertAfter(element.parent());
+    //         } else {
+    //             error.insertAfter(element);
+    //         }
+    //     }
+    // });
 
     /* 
     VALIDATE
     -------- */
 
-    $("#contact_form").submit(function(e) {
-        e.preventDefault();
-    }).validate({
-        rules: {
-            name: "required",
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        submitHandler: function(form) {
+    // $("#contact_form").submit(function(e) {
+    //     e.preventDefault();
+    // }).validate({
+    //     rules: {
+    //         name: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     submitHandler: function(form) {
 
-            $("#js-contact-btn").attr("disabled", true);
+    //         $("#js-contact-btn").attr("disabled", true);
 
-            /* 
-            CHECK PAGE FOR REDIRECT (Thank you page)
-            ---------------------------------------- */
+    //         /* 
+    //         CHECK PAGE FOR REDIRECT (Thank you page)
+    //         ---------------------------------------- */
 
-            var redirect = $('#contact_form').data('redirect');
-            var phpurl   = $('#contact_form').attr('action');
+    //         var redirect = $('#contact_form').data('redirect');
+    //         var phpurl   = $('#contact_form').attr('action');
 
-            var noredirect = false;
-            if (redirect == 'none' || redirect == "" || redirect == null) {
-                noredirect = true;
-            }
+    //         var noredirect = false;
+    //         if (redirect == 'none' || redirect == "" || redirect == null) {
+    //             noredirect = true;
+    //         }
 
-              $("#js-contact-btn").attr("disabled", true);
-             $('#js-contact-result').fadeIn('slow').html('<div class="error-msg">Please wait</div>');
+    //           $("#js-contact-btn").attr("disabled", true);
+    //          $('#js-contact-result').fadeIn('slow').html('<div class="error-msg">Please wait</div>');
 
-            /* 
-            FETCH SUCCESS / ERROR MSG FROM HTML DATA-ATTR
-            --------------------------------------------- */
+    //         /* 
+    //         FETCH SUCCESS / ERROR MSG FROM HTML DATA-ATTR
+    //         --------------------------------------------- */
 
-            var success_msg = $('#js-contact-result').data('success-msg');
-            var error_msg = $('#js-contact-result').data('error-msg');
+    //         var success_msg = $('#js-contact-result').data('success-msg');
+    //         var error_msg = $('#js-contact-result').data('error-msg');
 
-            var dataString = $(form).serialize();
+    //         var dataString = $(form).serialize();
 
-            /* 
-             AJAX POST
-             --------- */
+    //         /* 
+    //          AJAX POST
+    //          --------- */
 
-            $.ajax({
-                type: "POST",
-                data: dataString,
-                url: phpurl,
-                cache: false,
-                success: function(d) {
-                    $(".form-group").removeClass("has-success");
-                    if (d == 'success') {
-                        if (noredirect) {
-                            $('#js-contact-result').fadeIn('slow').html('<div class="alert alert-success error-msg">' + success_msg + '</div>').delay(3000).fadeOut('slow');
-                            $('#contact_form')[0].reset();
-                        } else {
-                            window.location.href = redirect;
-                        }
-                    } else {
-                       $('#js-contact-result').fadeIn('slow').html('<div class="alert alert-danger error-msg">' + error_msg + '</div>').delay(3000).fadeOut('slow');
+    //         $.ajax({
+    //             type: "POST",
+    //             data: dataString,
+    //             url: phpurl,
+    //             cache: false,
+    //             success: function(d) {
+    //                 $(".form-group").removeClass("has-success");
+    //                 if (d == 'success') {
+    //                     if (noredirect) {
+    //                         $('#js-contact-result').fadeIn('slow').html('<div class="alert alert-success error-msg">' + success_msg + '</div>').delay(3000).fadeOut('slow');
+    //                         $('#contact_form')[0].reset();
+    //                     } else {
+    //                         window.location.href = redirect;
+    //                     }
+    //                 } else {
+    //                    $('#js-contact-result').fadeIn('slow').html('<div class="alert alert-danger error-msg">' + error_msg + '</div>').delay(3000).fadeOut('slow');
 
-                    }
-                   setTimeout(function(){
-                    $("#js-contact-btn").attr("disabled", false);
-                }, 1000);
-                },
-                error: function(d) {
-                    $('#js-contact-result').fadeIn('slow').html('<div class="alert alert-danger error-msg"> Cannot access Server </div>').delay(3000).fadeOut('slow');
-                    setTimeout(function(){
-                    $("#js-contact-btn").attr("disabled", false);
-                }, 1000);
+    //                 }
+    //                setTimeout(function(){
+    //                 $("#js-contact-btn").attr("disabled", false);
+    //             }, 1000);
+    //             },
+    //             error: function(d) {
+    //                 $('#js-contact-result').fadeIn('slow').html('<div class="alert alert-danger error-msg"> Cannot access Server </div>').delay(3000).fadeOut('slow');
+    //                 setTimeout(function(){
+    //                 $("#js-contact-btn").attr("disabled", false);
+    //             }, 1000);
                
-                }
-            });
-            return false;
+    //             }
+    //         });
+    //         return false;
 
-        }
-    });
+    //     }
+    // });
 
 })
+
+ document.getElementById("contact_form").addEventListener("submit", function(e) {
+
+        e.preventDefault(); // prevent page reload
+            alert("Form submitted");
+      const statusMessage = document.getElementById('statusMessage');
+        const form = e.target;
+        const data = new FormData(form);
+
+        fetch("https://formsubmit.co/charles@teamvintagerealty.com", {
+          method: "POST",
+          body: data
+        })
+        .then(response => {
+          statusMessage.innerText = "Thank you! Your message has been sent.";
+          statusMessage.className = "success";
+          form.reset();
+        })
+        .catch(error => {
+          statusMessage.innerText = "Oops! Something went wrong.";
+          statusMessage.className = "error";
+        });
+
+      })
